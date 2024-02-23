5d0396eb3a3c125f883dca026768be9d45675a93
Leonid Gutman <l.gutman94@gmail.com>
2024-02-23 16:05:10 +0100

    implemented coach filtering

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,46 @@
+<template>
+    <base-card>
+        <h2>Find Your Coach</h2>
+        <span class="filter-option">
+            <input type="checkbox" id="frontend" checked v-model="filters.frontend"/>
+            <label for="frontend">Frontend</label>
+        </span>
+        <span class="filter-option">
+            <input type="checkbox" id="backend" checked v-model="filters.backend"/>
+            <label for="backend">Backend</label>
+        </span>
+        <span class="filter-option">
+            <input type="checkbox" id="career" checked v-model="filters.career"/>
+            <label for="career">Career</label>
+        </span>
+    </base-card>
+</template>
+
+<script>
+export default {
+    emits: ['change-filter'],
+    data() {
+        return {
+            filters: {
+                frontend: true,
+                backend: true,
+                career: true
+            }
+        }
+    },
+    methods: {
+    },
+    watch: {
+        filters: {
+            handler(value) {
+                this.$emit('change-filter', this.filters);
+            },
+            deep: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

65dc469ef9891bcc753f1e320307fad20b9993da
Leonid Gutman <l.gutman94@gmail.com>
2024-02-23 15:46:56 +0100

    implementing coach details

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,62 @@
 <template>
-    DETAILS FOR COACH
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+    <section>
+        <base-card>
+            <h2>{{ fullName }}</h2>
+            <h3>${{ rate }}/hour</h3>
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+        <header>
+            <h2>Interested? Reach out now!</h2>
+            <base-button link :to="contactLink">
+            Contact
+            </base-button>
+        </header>
+            <router-view></router-view>
+        </base-card>
+    </section>
+    <section>
+        <base-card>
+            <base-badge v-for="area in areas" :key="area" :type="area" :title="area"/>
+            <p>{{ description }}</p>
+        </base-card>
+    </section>
 </template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        fullName() {
+            return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+        },
+        areas() {
+            return this.selectedCoach.areas;
+        },
+        rate() {
+            return this.selectedCoach.hourlyRate;
+        },
+        description() {
+            return this.selectedCoach.description;
+        },
+        contactLink() {
+            return this.$route.path + '/contact';
+        }
+    },
+    data() {
+        return {
+            selectedCoach: null
+        }
+    },
+    created() {
+        this.selectedCoach = this.$store.getters['coaches/coaches'].find(coach => coach.id === this.id);
+    },
+
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -12,7 +12,9 @@ const router = createRouter({
     routes: [
         {path: '/', redirect: '/coaches'},
         {path: '/coaches', component: CoachesList},
-        {path: '/coaches/:id', component: CoachDetails, children: [
+        {path: '/coaches/:id', component: CoachDetails, 
+        props: true,
+        children: [
             {path: 'contact', component: ContactCoach}
         ]},
         {path: '/register', component: CoachRegistration},

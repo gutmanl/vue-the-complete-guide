f9a9bd767274ae3b24b518910e74fe0452935655
Leonid Gutman <l.gutman94@gmail.com>
2024-03-01 16:39:24 +0100

    added proper NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-    <div></div>
+    <section>
+        <base-card>
+            <h2>Page not found!</h2>
+            <p>
+                Sorry dude - we don't seem to have what you're looking for. Maybe check out our 
+                <router-link to="/coaches">coaches</router-link> instead?
+            </p>
+        </base-card>
+    </section>
 </template>

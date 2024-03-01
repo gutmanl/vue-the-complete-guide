918694d0c47322d1a6972e2318dcb3b705bfacda
Leonid Gutman <l.gutman94@gmail.com>
2024-03-01 14:03:34 +0100

    added requests prototype

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,49 @@
 <template>
-    <div>REQUESTS</div>
+    <section>
+        <base-card>
+            <header>
+                <h2></h2>
+            </header>
+            <ul v-if="hasRequests">
+                <RequestItem v-for="request in receivedRequests" :key="request.id"
+                :email="request.userEmail" :message="request.message" />
+            </ul>
+            <h3 v-else>No requests received so far</h3>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+    components: {
+        RequestItem
+    },
+    computed: {
+        receivedRequests() {
+            return this.$store.getters['requests/requests'];
+        },
+        hasRequests() {
+            return this.$store.getters['requests/hasRequests'];
+        }
+    }
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
